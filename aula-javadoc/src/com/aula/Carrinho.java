aa2638bd3c9591a0bbae1cc028d48a7f3ff27b5d
Eduarda Ferrazza <eduarda_stein@estudante.sesisenai.org.br>
2025-11-25 20:11:48 -0300

    Introdução ao Javadoc

---
+++ aula-javadoc/src/com/aula/Carrinho.java
@@ -0,0 +1,18 @@
+package com.aula;
+
+/**
+ * Representa o carrinho com produtos escolhidos pelo cliente.
+ * */
+public class Carrinho {
+
+    /**
+     * Calcula o total com base no produto e quantidade.
+     * @param produto produto a ser adiconado.
+     * @param quantidade quantidade do produto.
+     * @return valor total da compra.
+     * @see Produto#aplicarDesconto(double)
+     * */
+    public double calcularTotal(Produto produto, int quantidade){
+        return produto.aplicarDesconto(0) * quantidade;
+    }
+}
